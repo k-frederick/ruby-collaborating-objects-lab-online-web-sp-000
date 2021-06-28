435815bc891ff910db1395b9be5607ce4753591d
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-28 03:47:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,10 +14,6 @@ class Song
   new_song
   end
   def artist_name=(name)
-    if (self.artist.nil?)
-		  self.artist = Artist.find_or_create_by_name(name)
-	  else
-		  self.artist.name = name
-	  end
+  
   end
 end

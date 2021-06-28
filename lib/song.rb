c43c45d0509aaa391448c6a46e67506b051f74d2
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-28 03:40:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,8 @@ class Song
     @@all
   end
   def self.new_by_filename(filename)
-    song = file_name.split(" - ")[1]
-  artist = file_name.split(" - ")[0]
+    song = filename.split(" - ")[1]
+  artist = filename.split(" - ")[0]
   new_song = self.new(song)
   new_song.artist_name = artist
   new_song

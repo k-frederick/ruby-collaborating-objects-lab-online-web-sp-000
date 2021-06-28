eb0929feb77b57f5380a3ebf78a1cd80be6b7fc4
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-28 03:40:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,11 @@ class Song
     @@all
   end
   def self.new_by_filename(filename)
-
+    song = file_name.split(" - ")[1]
+  artist = file_name.split(" - ")[0]
+  new_song = self.new(song)
+  new_song.artist_name = artist
+  new_song
   end
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
